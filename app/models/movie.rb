c4caba1e449f2abe8323078b7d2f0d237efbdcf7
Ernst <ernsta@28cfe91ab2bd.ant.amazon.com>
2014-06-25 21:18:06 -0700

    Fourth commit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ActiveRecord::Base
+   
+    validates :title, :description, :year_released, presence: true
+    validates :description, length: { minimum: 10 }
+    validates :year_released, numericality: {only_integer: true }
+    validates :year_released, numericality: {greater_than: 0 }
 
 	def self.search_for(query)
 		where('title LIKE :query OR year_released LIKE :query OR description LIKE :query', query: "%#{query}%")
